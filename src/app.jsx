2e70b37bdf18ba14ac4c8646dcc3d1deb0dd5b07
Rahul Sompuram <sompuram@sas.upenn.edu>
2018-07-16 16:37:16 -0700

    toolbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import MyEditor from './components/MyEditor'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: 'MyEditor' };
+    this.redirect = this.redirect.bind(this);
+  }
+
+  redirect(page) {
+    this.setState({ currentPage: page });
+  }
+
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-      <RaisedButton color="primary">Bold</RaisedButton>
-    </div>);
+    return (
+      <div>
+        {this.state.currentPage === "MyEditor" ? <MyEditor /> : null}
+      </div>);
   }
 }
--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import { Editor, EditorState, RichUtils } from 'draft-js';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class MyEditor extends React.Component {
+const styleMap = {
+  'UPPERCASE': {
+    textTransform: 'uppercase'
+  },
+  'LOWERCASE': {
+    textTransform: 'lowercase'
+  }
+}
+
+export default class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
     this.onChange = editorState => this.setState({ editorState });
   }
 
-  onBoldClick(e) {
-    e.preventDefault()
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  toggleInlineStyle(e, inlineStyle) {
+    e.preventDefault();
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle));
   }
 
   render() {
-    return (<div>
-      <button onMouseDown={e => this.onBoldClick(e)}>BOLD</button>
-      <Editor
-        editorState={this.state.editorState}
-        onChange={this.onChange}
-      />
-    </div>);
+    return (
+      <div>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'BOLD')} color="primary">B</RaisedButton>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'ITALIC')} color="primary">I</RaisedButton>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UNDERLINE')} color="primary">U</RaisedButton>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'STRIKETHROUGH')} color="primary">S</RaisedButton>
+
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UPPERCASE')} color="primary">ABC</RaisedButton>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'LOWERCASE')} color="primary">abc</RaisedButton>
+
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'BOLD')} color="primary">Font color</RaisedButton>
+        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'ITALIC')} color="primary">Italic </RaisedButton>
+
+        <div className="editor">
+          <Editor
+            editorState={this.state.editorState}
+            customStyleMap={styleMap}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>);
   }
 }
