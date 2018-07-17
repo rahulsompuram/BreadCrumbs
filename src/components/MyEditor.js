d92e584daa8259a894c0d2bbc0ed565410b3e464
Rahul Sompuram <sompuram@sas.upenn.edu>
2018-07-16 17:15:18 -0700

    toolbar

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Editor, EditorState, RichUtils } from 'draft-js';
 import RaisedButton from 'material-ui/RaisedButton';
+import ColorPicker, { colorPickerPlugin } from 'draft-js-color-picker';
+
 
 const styleMap = {
   'UPPERCASE': {
@@ -11,11 +13,31 @@ const styleMap = {
   }
 }
 
+const presetColors = [
+  '#ff00aa',
+  '#F5A623',
+  '#F8E71C',
+  '#8B572A',
+  '#7ED321',
+  '#417505',
+  '#BD10E0',
+  '#9013FE',
+  '#4A90E2',
+  '#50E3C2',
+  '#B8E986',
+  '#000000',
+  '#4A4A4A',
+  '#9B9B9B',
+  '#FFFFFF',
+];
+
 export default class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
     this.onChange = editorState => this.setState({ editorState });
+    this.getEditorState = () => this.state.editorState;
+    this.picker = colorPickerPlugin(this.onChange, this.getEditorState);
   }
 
   toggleInlineStyle(e, inlineStyle) {
@@ -24,18 +46,28 @@ export default class MyEditor extends React.Component {
   }
 
   render() {
+    const { editorState } = this.state;
+    const inlineStyles = this.picker.exporter(editorState);
+
     return (
       <div>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'BOLD')} color="primary">B</RaisedButton>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'ITALIC')} color="primary">I</RaisedButton>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UNDERLINE')} color="primary">U</RaisedButton>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'STRIKETHROUGH')} color="primary">S</RaisedButton>
+        <div className="toolbar">
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'BOLD')} color="primary">B</RaisedButton>
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'ITALIC')} color="primary">I</RaisedButton>
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UNDERLINE')} color="primary">U</RaisedButton>
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'STRIKETHROUGH')} color="primary">S</RaisedButton>
 
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UPPERCASE')} color="primary">ABC</RaisedButton>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'LOWERCASE')} color="primary">abc</RaisedButton>
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'UPPERCASE')} color="primary">ABC</RaisedButton>
+          <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'LOWERCASE')} color="primary">abc</RaisedButton>
 
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'BOLD')} color="primary">Font color</RaisedButton>
-        <RaisedButton onMouseDown={e => this.toggleInlineStyle(e, 'ITALIC')} color="primary">Italic </RaisedButton>
+          <ColorPicker
+            toggleColor={color => this.picker.addColor(color)}
+            presetColors={presetColors}
+            color={this.picker.currentColor(editorState)}
+          />
+          <RaisedButton onMouseDown={this.picker.removeColor} color="primary">x</RaisedButton>
+
+        </div>
 
         <div className="editor">
           <Editor
