374c8e0921e918143c8626f99061d5c11360ff65
Rahul Sompuram <sompuram@sas.upenn.edu>
2018-07-18 09:07:19 -0700

    registration

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -19,7 +19,7 @@ export default class RegistrationPage extends React.Component {
         <div id="register_inputs">
           <Input focus type="text" placeholder='First Name' /> <br />
           <Input focus type="text" placeholder='Last Name' /> <br />
-          <Input focus type="text" placeholder='username' /> <br />
+          <Input focus type="text" placeholder='Username' /> <br />
           <Input focus type="password" placeholder='Password' /> <br />
           <Input focus type="password" placeholder='Repeat Password' /> <br />
           <Button primary>Register</Button>
