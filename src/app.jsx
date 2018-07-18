5a985f1761d0717939339d2b901334aa8419d999
whash123 <whash@tulane.edu>
2018-07-18 13:20:05 -0700

    register enter button functionality

--- src/app.jsx
+++ src/app.jsx
@@ -24,7 +24,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPage: 'LoginPage',
+      currentPage: 'RegistrationPage',
     };
     this.redirect = this.redirect.bind(this);
   }
