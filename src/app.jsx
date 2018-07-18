38c79c8ff2865b73990e596a81cff3567539cb5b
whash123 <whash@tulane.edu>
2018-07-18 11:25:24 -0700

    added save button

--- src/app.jsx
+++ src/app.jsx
@@ -10,7 +10,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPage: 'DocumentsPortal',
+      currentPage: 'MyEditor',
     };
     this.redirect = this.redirect.bind(this);
   }
